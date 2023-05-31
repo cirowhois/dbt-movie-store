648cab87f1d995ac59562bf9ebcb380b67f90c37
Ciro Ruiz <ciro.ruiz95@gmail.com>
2023-05-31 14:40:18 +0000

    dims/fct  improvements

--- models/movie_store/dim_movie_store__pay_films.sql
+++ models/movie_store/dim_movie_store__pay_films.sql
@@ -10,7 +10,7 @@ select
     film_rating,
     film_rep_cost,
     count(distinct(rental_id)) as rental_count,
-    sum(total_amount) as total_amount,
+    sum(amount_in_dollars) as total_amount,
     count(case when is_delayed = 'yes' then 1 else null end) as delayed_rentals,
     count(case when is_delayed = 'no' then 1 else null end) as ontime_rentals
 from {{ ref("fct_movie_store__payments") }}
--- models/movie_store/fct_movie_store__payments.sql
+++ models/movie_store/fct_movie_store__payments.sql
@@ -19,14 +19,15 @@ with
         select
             {{
                 dbt_utils.generate_surrogate_key(
-                    ["r.customer_id", "r.rental_id", "r.film_id", "r.store_id"]
+                    ["r.payment_id","r.customer_id", "r.rental_id", "r.film_id", "r.store_id"]
                 )
             }} as payments_sk
 
             -- payment
-            , sum(r.amount_in_dollars) as total_amount
-            , count(distinct(r.payment_id)) as total_payments
-
+            , r.payment_id
+            , r.amount_in_dollars
+            , r.payment_date_at
+            
             -- rental
             , r.rental_id
             , r.rental_date_at
@@ -42,6 +43,7 @@ with
             , r.customers_name
             , l.city as customer_city
             , l.country as customer_country
+            , r.customer_created_date
 
             -- films
             , r.film_id
@@ -71,7 +73,7 @@ with
         left join films f on r.film_id = f.film_id
         left join stores s on r.store_id = s.store_id
         left join location lc on s.address_id = lc.address_id
-        group by 1,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31
+        
     )
 
 -- final query
